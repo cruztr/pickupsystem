e897d9099f7cea46f68d19a63e9dc5a45e305cbf
TREVOR JOVE CRUZ (SHP-IRIS-ISD-OOCLL/MNL) <CRUZTR@OOCL.COM>
2019-10-16 15:08:56 +0800

    [TJ&KEN] Added CompanyProfile and OneToOne relationship

--- src/main/java/com/tw/apistackbase/core/Company.java
+++ src/main/java/com/tw/apistackbase/core/Company.java
@@ -1,9 +1,17 @@
 package com.tw.apistackbase.core;
 
+import javax.persistence.*;
+
+@Entity
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private CompanyProfile profile;
 
     public Long getId() {
         return id;
@@ -19,6 +27,14 @@ public class Company {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public CompanyProfile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(CompanyProfile profile) {
+        this.profile = profile;
     }
 
     public Company() {
